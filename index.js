d6d5e65c80ff118e513df938838cbe85c88a3308
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-04 00:00:46 +1000

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const bodyElement = document.querySelector('body');
+
+
+bodyElement.querySelector('main').remove();
+
+const newHeader = document.querySelector('h1#victory');
+
